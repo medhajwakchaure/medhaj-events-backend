48e9e2dfa726a56cdb19cbeaf726c1586d479f71
Medhaj Wakchaure <medhajwakchaure002@gmail.com>
2025-11-11 20:34:52 +0530

    chore: initialize backend repository (gitignore, editorconfig, readme)

---
+++ src/main/java/com/medhajevents/backend/MedhajEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medhajevents.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedhajEventsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedhajEventsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medhajevents/backend/MedhajEventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhajevents.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedhajEventsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
